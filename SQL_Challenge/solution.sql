2a6f9f7d2e6b03805bcd94d9a19f62a84425def0
Satyam <satyam6111991@gmail.com>
2022-01-23 14:30:41 +0530

    Committing SQL Challenge

--- SQL_Challenge/solution.sql
+++
@@ -1,6 +0,0 @@
---1. Given an IMPRESSIONS table with product_id, click (an indicator that the product was clicked), and date,
---   write a query that will tell you the click-through-rate of each product by month
-
-
-
-
